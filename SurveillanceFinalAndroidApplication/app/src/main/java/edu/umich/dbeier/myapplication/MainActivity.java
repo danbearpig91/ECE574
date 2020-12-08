db206478e1423427ccaae9b10181757812290ab3
PF0WHFBF\Veteran <dbeier@umich.edu>
2020-12-07 20:27:51 -0500

    Android Application

---
+++ SurveillanceFinalAndroidApplication/app/src/main/java/edu/umich/dbeier/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package edu.umich.dbeier.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://proxy21.rt3.io:35701/stream/video.mjpeg");
+    }
+}
